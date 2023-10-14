d996816c58f8b59f8feabc6bbefd9eea5c0a86df
bryanstithquintero <bryanquinterog@gmail.com>
2023-10-14 15:59:06 -0500

    add the about section

--- src/container/About/About.jsx
+++ src/container/About/About.jsx
@@ -1,11 +1,45 @@
 import './About.scss'
 
-const About = () => {
-    return (
-        <div>
-            <p>About</p>
-        </div>
-    )
-}
+import { motion } from 'framer-motion';
+import { images } from '../../constants'
+//import { AppWrap, MotionWrap } from '../../wrapper';
 
-export default About
+
+const About = () => {
+    const abouts = [
+        {
+            title: "Backend development",
+            imgUrl: images.about01,
+            description: "esto soy para describir"
+        },
+        {
+            title: "sql",
+            imgUrl: images.about02,
+            description: "otra description"
+        }
+    ]
+    return (
+        < >
+            <h2 className="head-text">I Know that <span>Good Development</span> <br />means  <span>Good Apps</span></h2>
+
+            <div id='about' className="app__profiles">
+                {abouts.map((about, index) => (
+                    <motion.div
+                        whileInView={{ opacity: 1 }}
+                        whileHover={{ scale: 1.1 }}
+                        transition={{ duration: 0.5, type: 'tween' }}
+                        className="app__profile-item"
+                        key={about.title + index}
+                    >
+                        <img src={about.imgUrl} alt={about.title} />
+                        <h2 className="bold-text" style={{ marginTop: 20 }}>{about.title}</h2>
+                        <p className="p-text" style={{ marginTop: 10 }}>{about.description}</p>
+                    </motion.div>
+                ))}
+            </div>
+        </>
+    );
+};
+
+
+export default About;
